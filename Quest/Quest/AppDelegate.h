c77dbb7446313bd8560bee1074e5e818a879702f
Greg Tropino <gatropino@gmail.com>
2014-05-06 19:14:51 -0500

    uploading blank project

---
+++ Quest/Quest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quest
+//
+//  Created by Greg Tropino on 5/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quest/Quest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quest
+//
+//  Created by Greg Tropino on 5/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
